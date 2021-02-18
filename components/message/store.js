7fd2098103193ec17fd365a78ded01a140382c6a
Carlos yaco <cyaco33@gmail.com>
2021-02-18 15:35:18 -0500

    developed connection mongo db

--- components/message/store.js
+++ components/message/store.js
@@ -1,9 +1,7 @@
-const db = require('mongoose');
+
 const Model = require('./model')
 
-db.Promise = global.Promise; //BP para acceder a la clase promise 
-db.connect('mongodb+srv://cyaco33:jobsMANCH1955@cluster0.k1i5c.mongodb.net/test',{useNewUrlParser:true,});
-console.log('[db] conectada con exito');
+
 function addMessage(message){
   //list.push(message);
   const myMessage =  new Model(message);
