76c967a96cfe52fee58e35574d74aa6fe2a45dfd
Carlos yaco <cyaco33@gmail.com>
2021-05-08 16:13:04 -0500

    update

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();//inicializar express
 const server = require('http').Server(app); //configurando los websockets
 
+const cors = require('cors');//para habilitar las cabeceras
 const db = require('./db');
 const socket = require('./socket');////configurando los websockets
 const router = require('./network/routes');
 //conectar a la base de datos
 db('mongodb+srv://cyaco33:jobsMANCH1955@cluster0.k1i5c.mongodb.net/test');
 
+app.use(cors());
 app.use(express.json());//para utilizar el body-parse que biene incluido en exprres
 app.use('/app',express.static('public'));
 //inicializar socket
